a08a4f5dfae92996f27965915c692927ef4e2b4c
Maycon Barboza <mayconbarboza20@gmail.com>
2025-08-24 20:26:54 -0300

    criacao de classes

---
+++ Models/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursos_c_.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.writeline($"ola, meu nome é {Nome}, e tenho {Idade} anos");
+            
+        }
+    }
+}
